8a3a515220da16375d2b10483b54428f61c94504
AnonymousManan <mananaggarwal2001@gmail.com>
2024-03-04 19:43:51 +0530

    add new employee and list employee done

---
+++ src/main/java/com/luv2code/springboot/cruddemo/DemoAppThymeleaf.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppThymeleaf {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppThymeleaf.class, args);
+	}
+
+}
